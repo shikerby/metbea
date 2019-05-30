8a98079b9a6e3ff5a2b38a1f94b5207516dff3bf
shikerby <shikerby@gmail.com>
2019-05-30 16:47:58 +0800

    static root

--- metmsg/settings.py
+++ metmsg/settings.py
@@ -128,9 +128,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
